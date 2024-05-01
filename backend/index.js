1dca0a48b41496cb853e104233fafc5807b02153
AbhishekRH <abhishek7.rh@gmail.com>
2024-05-01 23:52:05 +0530

    vercel-deployed-5

--- backend/index.js
+++ backend/index.js
@@ -43,6 +43,17 @@ app.use(
     credentials: true,
   })
 );
+
+// Enable CORS for all origins
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/files", express.static("files"));
 
 //mongodb connection----------------------------------------------
