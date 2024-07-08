50732a32fdfe8d53a39c1dc1f11e1f332bea8e68
AbhishekRH <abhishek7.rh@gmail.com>
2024-07-08 17:15:32 +0530

    removed unnecessary comments

--- backend/index.js
+++ backend/index.js
@@ -44,22 +44,6 @@ const corsConfig = {
 app.options("", cors(corsConfig));
 app.use(cors(corsConfig));
 
-// {
-//   origin: ["*"],
-//   methods: ["GET", "POST"],
-//   credentials: true,
-// }
-
-// Enable CORS for all origins
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-
 // Enable CORS for all routes
 app.use((req, res, next) => {
   res.setHeader(
@@ -85,13 +69,6 @@ mongoose
     console.log("Connected to database");
   })
   .catch((e) => console.log(e));
-
-// app.use((req, res, next) => {
-//   if (req.session.userID && req.cookies.user_sid) {
-//     res.redirect("/");
-//   }
-//   next();
-// });
 
 const isAuth = (req, res, next) => {
   if (req.session.isAuth) {
@@ -128,75 +105,13 @@ const redirectHome = (req, res, next) => {
   }
 };
 
-// app.get("/", sessionChecker, (req, res) => {
-//   res.redirect("/");
-// });
-
-// app.use(async (req, res, next) => {
-//   const { userId } = req.session;
-//   if (userId) {
-//     try {
-//       const user = await User.findOne({ _id: userId });
-//       if (user) {
-//         res.locals.user = user;
-//         console.log("heheheheehhehe" + res.locals.user);
-//       }
-//     } catch (error) {
-//       console.error("Error fetching user:", error);
-//     }
-//   }
-//   next();
-// });
-
-// Middleware to set session data to res.locals
-// app.use((req, res, next) => {
-//   res.locals.sessionData = req.session;
-//   next();
-// });
-
-// Now you can access session data in any route handler
-// app.get("/example-route", (req, res) => {
-//   const sessionData = res.locals.sessionData;
-//   // Access specific session properties, e.g., email
-//   const userEmail = sessionData.email;
-//   res.send(`User email: ${userEmail}`);
-// });
-
-// // Another route where you can access session data
-// app.get("/another-route", (req, res) => {
-//   const sessionData = res.locals.sessionData;
-//   // Access other session properties as needed
-//   res.send(`Session data: ${JSON.stringify(sessionData)}`);
-// });
-
 //loginBACKEND ---------------------------------------------------------------
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
-  // tokens: [
-  //   {
-  //     token: {
-  //       type: String,
-  //       required: true,
-  //     },
-  //   },
-  // ],
 });
-
-// generating tokens
-// userSchema.methods.generateAuthToken = async function () {
-//   try {
-//     const token = jwt.sign(
-//       { _id: this._id.toString() },
-//       process.env.SECRET_KEY
-//     );
-//     this.tokens = this.tokens.concat({ token: token });
-//     await this.save();
-//     return token;
-//   } catch (error) {}
-// };
 
 //multer------------------------------------------------------------
 const multer = require("multer");
@@ -218,37 +133,6 @@ const PdfSchema = mongoose.model("PdfDetails");
 const User = new mongoose.model("User", userSchema);
 const reviewer = new mongoose.model("Reviewer", userSchema);
 
-// import react from "../WT_Course_Project/src/components/Login"
-
-//---------------ROUTES-----------------
-// app.get("/logout", (req, res) => {
-//   if (req.session.userID && req.cookies.user_sid) {
-//     req.session.destroy((err) => {
-//       if (err) {
-//         res.status(400).send("Unable to log out");
-//       } else {
-//         res.send("Logout successful");
-//       }
-//     });
-
-//     res.clearCookie("user_sid");
-//     res.redirect("/login");
-//   } else {
-//     res.redirect("/login");
-//   }
-// });
-
-// app.post("/logout", (req, res) => {
-//   req.session.destroy((err) => {
-//     if (err) {
-//       return res.redirect("/");
-//     } else {
-//       res.clearCookie("user_sid");
-//       res.redirect("/login");
-//     }
-//   });
-// });
-
 app
   .route("/login")
   .get(redirectHome, (req, res) => {
@@ -263,14 +147,6 @@ app
       const { email, password } = req.body;
       const user = await User.findOne({ email: email });
       const pdf = await PdfSchema.findOne({ email: email });
-
-      // const token = await user.generateAuthToken(); // CALLING generateAuthToken() function for /login page
-      // console.log(token);
-
-      // res.cookie("jwt", token, {
-      //   maxAge: 60000,
-      //   httpOnly: true,
-      // });
 
       if (user) {
         if (password === user.password) {
@@ -287,13 +163,6 @@ app
           console.log("session user ID: " + req.session.userID);
           console.log("current user's email: " + req.session.email);
           console.log("session ID: " + req.sessionID);
-
-          // Set res.locals.user directly
-          // res.locals.user = user;
-          // console.log(res.locals.user.email);
-
-          // const LOLuser = res.locals.user;
-          // console.log("LOLuser's email: " + LOLuser.email);
 
           if (user.email === pdf?.email) {
             res.send({
@@ -326,8 +195,6 @@ app
       }
     } catch (error) {
       console.log(error.response);
-      // console.error("Error during login:", error);
-      // res.status(500).send({ message: "Internal Server Error" });
     }
   });
 
@@ -353,14 +220,6 @@ app
           email,
           password,
         });
-
-        // const token = await newUser.generateAuthToken(); // CALLING generateAuthToken() function for /register page
-        // console.log("TOKEN : " + token);
-
-        // res.cookie("jwt", token, {
-        //   maxAge: 60000,
-        //   httpOnly: true,
-        // });
 
         await newUser.save((err, docs) => {
           if (err) {
@@ -399,12 +258,9 @@ app
             });
           }
         });
-
-        // res.send({ message: "Successfully Registered, Please login now." });
       }
     } catch (error) {
       console.error("Error during registration:", error);
-      // console.log(error.response);
       res.status(500).send({ message: "Internal Server Error" });
     }
   });
@@ -427,8 +283,6 @@ app.post("/login_rev", redirectHome, async (req, res) => {
     }
   } catch (error) {
     console.log(error.response);
-    // console.error("Error during login:", error);
-    // res.status(500).send({ message: "Internal Server Error" });
   }
 });
 
@@ -441,8 +295,6 @@ app.post("/upload-files", upload.single("file"), async (req, res) => {
   console.log("Request email:", req.body.email);
   console.log("Request file/pdf:", req.file.filename);
 
-  // const email = req.body.email;
-  // const userEmail = "userEmail";
   const title = req.body.title;
   const userEmail = req.body.email;
   const fileName = req.file.filename;
@@ -469,41 +321,6 @@ app.get("/get-files", async (req, res) => {
     });
   } catch (error) {}
 });
-
-// app.get("/get-files", async (req, res) => {
-//   try {
-//     // const userEmail = req.session.email; // Assuming user's email is stored in the session
-//     // console.log("tttttttttt " + userEmail);
-//     // const userEmail = res.locals.user.email;
-//     // if (!userEmail) {
-//     //   return res.status(401).send({ status: "error", message: "Unauthorized" });
-//     // }
-
-//     // const sessionData = res.locals.sessionData;
-//     // console.log("ddddddddd " + sessionData.email);
-
-//     // const userr = res.locals;
-//     // console.log("yyyyyyyy" + userr.email);
-
-//     // console.log("req . session" + req.session);
-//     // const userId = req.session.userID;
-//     // console.log("aaaaaaaaa " + userId);
-//     // const user = await User.findOne({ _id: userId });
-//     // console.log("bbbbbbbbb " + user);
-//     // const userEmail = user.email;
-//     // console.log("ccccccccc " + userEmail);
-
-//     // const userEmail = user.email;
-//     // console.log("aa gaya email: " + userEmail)
-
-//     PdfSchema.find({ email: userEmail }).then((data) => {
-//       res.send({ status: "ok", data: data });
-//     });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ status: "error", message: "Internal Server Error" });
-//   }
-// });
 
 app.post("/get-files", async (req, res) => {
   try {
@@ -558,32 +375,6 @@ app.post("/updateAttribute", async (req, res) => {
   }
 });
 
-// const createToken = async () => {
-//   const token = await jwt.sign(
-//     { _id: "12345678910" },
-//     "abhishekrajkumarabhishekrajkumar"
-//   );
-//   console.log(token);
-
-//   const userVer = await jwt.verify(token, "abhishekrajkumarabhishekrajkumar");
-//   console.log(userVer);
-// };
-
-// createToken();
-
-//apis----------------------------------------------------------------
-// app.get("/", async (req, res) => {
-//   res.send("Success!!!!!!");
-// });
-
-// app.get("/authorConsole", async (req, res) => {
-//   // try {
-//   //   if (!req.session.userID) return res.redirect("/login");
-//   // } catch (error) {
-//   //   res.send(error);
-//   // }
-// });
-
 app
   .route("/authorConsole")
   .get(isAuth, (req, res) => {
@@ -594,20 +385,6 @@ app
   .post();
 
 app.post("/logout", (req, res) => {
-  // try {
-  //   req.session.destroy((err) => {
-  //     if (err) {
-  //       console.error("Error destroying session:", err);
-  //       res.status(500).send("Unable to log out");
-  //     } else {
-  //       res.clearCookie("user_sid");
-  //       res.redirect("/login");
-  //     }
-  //   });
-  // } catch (error) {
-  //   console.error("Unexpected error during logout:", error);
-  //   res.status(500).send("Internal Server Error");
-  // }
   req.session.destroy((err) => {
     if (err) throw err;
     res.clearCookie("user_sid");
